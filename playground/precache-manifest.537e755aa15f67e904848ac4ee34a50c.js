e6dbf44c10e418daf55799aaf878f5802fbb0cfc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-31 18:26:31 +0000

    Deploy dprint.dev to github.com/dsherret/dprint.git:gh-pages

---
+++ playground/precache-manifest.537e755aa15f67e904848ac4ee34a50c.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa426e201061121c2f1959cfc2e81029",
+    "url": "/playground/index.html"
+  },
+  {
+    "revision": "869c3c4288f43e4a04c8",
+    "url": "/playground/static/css/0.411bffa9.chunk.css"
+  },
+  {
+    "revision": "429de463784afdee9563",
+    "url": "/playground/static/css/4.d4fceaf6.chunk.css"
+  },
+  {
+    "revision": "be7b00cc18473d73d6f9",
+    "url": "/playground/static/css/main.a3c22f11.chunk.css"
+  },
+  {
+    "revision": "869c3c4288f43e4a04c8",
+    "url": "/playground/static/js/0.77d517cf.chunk.js"
+  },
+  {
+    "revision": "de15cdbc446322a32225",
+    "url": "/playground/static/js/1.1c7a9dbc.chunk.js"
+  },
+  {
+    "revision": "ca844494bbd0d988cd90",
+    "url": "/playground/static/js/10.29861373.chunk.js"
+  },
+  {
+    "revision": "586adbbce407eba10acd",
+    "url": "/playground/static/js/11.559afaa2.chunk.js"
+  },
+  {
+    "revision": "58d5271744475c4aaa50",
+    "url": "/playground/static/js/12.5ca60fe6.chunk.js"
+  },
+  {
+    "revision": "9a6d41f44a1ca4fc6669",
+    "url": "/playground/static/js/13.c34aebe0.chunk.js"
+  },
+  {
+    "revision": "5df358faa8ac8ffbf353",
+    "url": "/playground/static/js/14.a7c14db4.chunk.js"
+  },
+  {
+    "revision": "827573964a57a376891a",
+    "url": "/playground/static/js/15.01dd3364.chunk.js"
+  },
+  {
+    "revision": "82cac47e3144750c93a3",
+    "url": "/playground/static/js/16.2aa47637.chunk.js"
+  },
+  {
+    "revision": "8398d18bfb6b26f36dc1",
+    "url": "/playground/static/js/17.3acec615.chunk.js"
+  },
+  {
+    "revision": "4968cd4d946787bd1445",
+    "url": "/playground/static/js/18.91e7b2ab.chunk.js"
+  },
+  {
+    "revision": "05efbfd770b873f3ed74",
+    "url": "/playground/static/js/19.0f2c3c39.chunk.js"
+  },
+  {
+    "revision": "3e39eff1e6fb76e9747a",
+    "url": "/playground/static/js/20.4712c9d8.chunk.js"
+  },
+  {
+    "revision": "6b268fd034394ac7788d",
+    "url": "/playground/static/js/21.5d90e11a.chunk.js"
+  },
+  {
+    "revision": "4fe83a474350444bb4a4",
+    "url": "/playground/static/js/22.b3797afe.chunk.js"
+  },
+  {
+    "revision": "5f3635ce4c3949647a39",
+    "url": "/playground/static/js/23.31cee9c2.chunk.js"
+  },
+  {
+    "revision": "4e570db711aecab2eebe",
+    "url": "/playground/static/js/24.fecf928a.chunk.js"
+  },
+  {
+    "revision": "45400ad8f8a5d05d3d65",
+    "url": "/playground/static/js/25.4d2ea4bc.chunk.js"
+  },
+  {
+    "revision": "c485154473f4d74cdfa3",
+    "url": "/playground/static/js/26.f1237d62.chunk.js"
+  },
+  {
+    "revision": "20784521aaa98dfd9c0d",
+    "url": "/playground/static/js/27.7db544df.chunk.js"
+  },
+  {
+    "revision": "6e309c2783ffb8e39688",
+    "url": "/playground/static/js/28.dc6b6120.chunk.js"
+  },
+  {
+    "revision": "f18a09dc0c8b523651e5",
+    "url": "/playground/static/js/29.4a88c65b.chunk.js"
+  },
+  {
+    "revision": "60b2d974dccaaa9e05a7",
+    "url": "/playground/static/js/30.b2020b71.chunk.js"
+  },
+  {
+    "revision": "5f6b8acc623e9cf30c9e",
+    "url": "/playground/static/js/31.c81bc6f9.chunk.js"
+  },
+  {
+    "revision": "cb3c311127cd686ddd42",
+    "url": "/playground/static/js/32.3c5f6ed4.chunk.js"
+  },
+  {
+    "revision": "6a642de0959c6f1d626b",
+    "url": "/playground/static/js/33.66b59ff2.chunk.js"
+  },
+  {
+    "revision": "0125da146a97a82714b5",
+    "url": "/playground/static/js/34.f3bb29c1.chunk.js"
+  },
+  {
+    "revision": "a1520dce6c0f74e3c913",
+    "url": "/playground/static/js/35.254da6b7.chunk.js"
+  },
+  {
+    "revision": "beb47ff8651f1c48dcac",
+    "url": "/playground/static/js/36.c8952779.chunk.js"
+  },
+  {
+    "revision": "83273e4ab3ef3edc2e16",
+    "url": "/playground/static/js/37.74c63079.chunk.js"
+  },
+  {
+    "revision": "2aaa41546bd6dc39e280",
+    "url": "/playground/static/js/38.42d629d7.chunk.js"
+  },
+  {
+    "revision": "e943ee09b92f05c55ea1",
+    "url": "/playground/static/js/39.06c90b09.chunk.js"
+  },
+  {
+    "revision": "429de463784afdee9563",
+    "url": "/playground/static/js/4.9cbca5db.chunk.js"
+  },
+  {
+    "revision": "531a922a8099acb21bcf",
+    "url": "/playground/static/js/40.fb397b03.chunk.js"
+  },
+  {
+    "revision": "81ef4fd3bc825ad224fb",
+    "url": "/playground/static/js/41.666bf212.chunk.js"
+  },
+  {
+    "revision": "3d75f9146f7b9c9122c5",
+    "url": "/playground/static/js/42.82773d0e.chunk.js"
+  },
+  {
+    "revision": "8a761c6467c404867514",
+    "url": "/playground/static/js/43.f771a34a.chunk.js"
+  },
+  {
+    "revision": "f48ae3ebe6b55af25c36",
+    "url": "/playground/static/js/44.1ef78643.chunk.js"
+  },
+  {
+    "revision": "989e0542cbdd2767a83d",
+    "url": "/playground/static/js/45.989f6bb0.chunk.js"
+  },
+  {
+    "revision": "ab5a975c8faa93e04a35",
+    "url": "/playground/static/js/46.ab092361.chunk.js"
+  },
+  {
+    "revision": "6116099bbda01844368f",
+    "url": "/playground/static/js/47.766a83d4.chunk.js"
+  },
+  {
+    "revision": "e783b4eb1c7d6064445f",
+    "url": "/playground/static/js/48.3499e849.chunk.js"
+  },
+  {
+    "revision": "2922ff8bb1927c33cbaf",
+    "url": "/playground/static/js/49.bb513b70.chunk.js"
+  },
+  {
+    "revision": "543fb15ae5703b8a3f8e",
+    "url": "/playground/static/js/5.d9ed3185.chunk.js"
+  },
+  {
+    "revision": "69a272317c0f0f777137",
+    "url": "/playground/static/js/50.a4dd70bb.chunk.js"
+  },
+  {
+    "revision": "0f6f8d8fe4b5e70f9ede",
+    "url": "/playground/static/js/51.1ffe2d52.chunk.js"
+  },
+  {
+    "revision": "506b3d129d221ac05446",
+    "url": "/playground/static/js/52.31f1be57.chunk.js"
+  },
+  {
+    "revision": "44c74ee04eaabb58c7d9",
+    "url": "/playground/static/js/53.7aada66d.chunk.js"
+  },
+  {
+    "revision": "fd34385926e455365236",
+    "url": "/playground/static/js/54.72d67df4.chunk.js"
+  },
+  {
+    "revision": "8c2bd56c86b732504767",
+    "url": "/playground/static/js/55.113ef012.chunk.js"
+  },
+  {
+    "revision": "db300d5bfc6f442756dc",
+    "url": "/playground/static/js/56.abf1993f.chunk.js"
+  },
+  {
+    "revision": "2b08055e999a4e0dade7",
+    "url": "/playground/static/js/57.96785372.chunk.js"
+  },
+  {
+    "revision": "4970390c2756458e91f3",
+    "url": "/playground/static/js/58.c760acbf.chunk.js"
+  },
+  {
+    "revision": "590518a564afce620392",
+    "url": "/playground/static/js/59.66d57313.chunk.js"
+  },
+  {
+    "revision": "3f43cbfa902ef96db6b0",
+    "url": "/playground/static/js/6.9258a09f.chunk.js"
+  },
+  {
+    "revision": "2344c1efa44890d6e92a",
+    "url": "/playground/static/js/60.bdd0254b.chunk.js"
+  },
+  {
+    "revision": "856d995fe022308658b7",
+    "url": "/playground/static/js/61.6486bd30.chunk.js"
+  },
+  {
+    "revision": "1fd767efbb7a03259542",
+    "url": "/playground/static/js/62.e201d596.chunk.js"
+  },
+  {
+    "revision": "4df4ca2dcf396e194424",
+    "url": "/playground/static/js/7.880ae9ad.chunk.js"
+  },
+  {
+    "revision": "e45350f1802ff11961bb",
+    "url": "/playground/static/js/8.d57988f6.chunk.js"
+  },
+  {
+    "revision": "10ea912fe90f548eeddb",
+    "url": "/playground/static/js/9.b70ce75c.chunk.js"
+  },
+  {
+    "revision": "be7b00cc18473d73d6f9",
+    "url": "/playground/static/js/main.b70d0e8e.chunk.js"
+  },
+  {
+    "revision": "9b661ce9e0d89a9f540e",
+    "url": "/playground/static/js/runtime~main.26a090ec.js"
+  }
+]);
--- playground/service-worker.js
+++ playground/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/playground/precache-manifest.32687bea7116bc6a5639633acf75505b.js"
+  "/playground/precache-manifest.537e755aa15f67e904848ac4ee34a50c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/playground/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
