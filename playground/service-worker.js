65300e52a738013a95b7789c77078c4d5ab88c67
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-15 00:01:21 +0000

    Deploy dprint.dev to github.com/dsherret/dprint.git:gh-pages

--- playground/service-worker.js
+++ playground/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/playground/precache-manifest.249e6ba46685f12a9b7f871dd3bcf44e.js"
+  "/playground/precache-manifest.ced77df59b3065c08feb483d838f4158.js"
 );
 
 self.addEventListener('message', (event) => {
